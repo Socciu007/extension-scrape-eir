cf72b7c2e6b732b687f8d431b92a6ccc41f54a56
Money <manhtien310701@gmail.com>
2026-03-20 15:01:16 +0800

    add complete-extension

--- src/components/SelectComponent.tsx
+++ src/components/SelectComponent.tsx
@@ -10,9 +10,9 @@ const SelectComponent = ({
   label: string;
 }) => {
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-2 items-center">
       <label htmlFor={id}>{label}</label>
-      <select id={id} onChange={onChange}>
+      <select className="border-1 border-gray-300 rounded-md p-2" id={id} onChange={onChange}>
         {options.map((option) => (
           <option key={option} value={option}>
             {option}
